c2215cd2d8fd4c1fdaebf6db5e77b9d09d5a93d8
Gary Donovan <gary.donovan@oovvuu.com>
2020-06-28 20:21:29 +1000

    Standard formatting for source code

--- src/xraysink/asgi/middleware.py
+++ src/xraysink/asgi/middleware.py
@@ -1,14 +1,16 @@
 """
 AioHttp Middleware
 """
+
 from aiohttp import web
 from aiohttp.web_exceptions import HTTPException
-
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core.models import http
 from aws_xray_sdk.core.utils import stacktrace
-from aws_xray_sdk.ext.util import calculate_sampling_decision, \
-    calculate_segment_name, construct_xray_header, prepare_response_header
+from aws_xray_sdk.ext.util import calculate_sampling_decision
+from aws_xray_sdk.ext.util import calculate_segment_name
+from aws_xray_sdk.ext.util import construct_xray_header
+from aws_xray_sdk.ext.util import prepare_response_header
 
 
 @web.middleware
@@ -19,19 +21,19 @@ async def middleware(request, handler):
     # Create X-Ray headers
     xray_header = construct_xray_header(request.headers)
     # Get name of service or generate a dynamic one from host
-    name = calculate_segment_name(request.headers['host'].split(':', 1)[0], xray_recorder)
+    name = calculate_segment_name(
+        request.headers["host"].split(":", 1)[0], xray_recorder
+    )
 
     sampling_req = {
-        'host': request.headers['host'],
-        'method': request.method,
-        'path': request.path,
-        'service': name,
+        "host": request.headers["host"],
+        "method": request.method,
+        "path": request.path,
+        "service": name,
     }
 
     sampling_decision = calculate_sampling_decision(
-        trace_header=xray_header,
-        recorder=xray_recorder,
-        sampling_req=sampling_req,
+        trace_header=xray_header, recorder=xray_recorder, sampling_req=sampling_req
     )
 
     # Start a segment
@@ -47,14 +49,14 @@ async def middleware(request, handler):
     segment.put_http_meta(http.URL, str(request.url))
     segment.put_http_meta(http.METHOD, request.method)
 
-    if 'User-Agent' in request.headers:
-        segment.put_http_meta(http.USER_AGENT, request.headers['User-Agent'])
+    if "User-Agent" in request.headers:
+        segment.put_http_meta(http.USER_AGENT, request.headers["User-Agent"])
 
-    if 'X-Forwarded-For' in request.headers:
-        segment.put_http_meta(http.CLIENT_IP, request.headers['X-Forwarded-For'])
+    if "X-Forwarded-For" in request.headers:
+        segment.put_http_meta(http.CLIENT_IP, request.headers["X-Forwarded-For"])
         segment.put_http_meta(http.X_FORWARDED_FOR, True)
-    elif 'remote_addr' in request.headers:
-        segment.put_http_meta(http.CLIENT_IP, request.headers['remote_addr'])
+    elif "remote_addr" in request.headers:
+        segment.put_http_meta(http.CLIENT_IP, request.headers["remote_addr"])
     else:
         segment.put_http_meta(http.CLIENT_IP, request.remote)
 
@@ -75,8 +77,8 @@ async def middleware(request, handler):
     finally:
         if response is not None:
             segment.put_http_meta(http.STATUS, response.status)
-            if 'Content-Length' in response.headers:
-                length = int(response.headers['Content-Length'])
+            if "Content-Length" in response.headers:
+                length = int(response.headers["Content-Length"])
                 segment.put_http_meta(http.CONTENT_LENGTH, length)
 
             header_str = prepare_response_header(xray_header, segment)
