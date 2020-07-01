3d69a7e2be90bcab9b7a2d69276fe123e44115e8
Gary Donovan <gary.donovan@oovvuu.com>
2020-07-02 09:25:03 +1000

    Move aiohttp app to a separate module

--- tests/asgi/middleware/test_middleware.py
+++ tests/asgi/middleware/test_middleware.py
@@ -6,14 +6,12 @@ import asyncio
 from unittest.mock import patch
 
 import pytest
-from aiohttp import web
-from aiohttp.web_exceptions import HTTPUnauthorized
 from aws_xray_sdk import global_sdk_config
 from aws_xray_sdk.core.async_context import AsyncContext
 from aws_xray_sdk.core.emitters.udp_emitter import UDPEmitter
 from aws_xray_sdk.core.models import http
-from aws_xray_sdk.ext.aiohttp.middleware import middleware
 
+from ._aiohttp import AioHttpServerFactory
 from ...xray_util import get_new_stubbed_recorder
 
 pytestmark = pytest.mark.asyncio
@@ -28,7 +26,7 @@ from aiohttp.pytest_plugin import aiohttp_client
 
 
 # Inject a `loop` fixture based on the normal `event_loop` fixture,
-# since aiohttp_client has this as a dependency
+# since aiohttp_client has this as a fixture dependency
 @pytest.fixture
 async def loop(event_loop):
     yield event_loop
@@ -40,68 +38,6 @@ async def client(event_loop, aiohttp_client):
     # a custom client (not the usual `async_asgi_testclient.AsyncTestClient`)
     client = await aiohttp_client(AioHttpServerFactory.app(loop=event_loop))
     yield client
-
-
-class AioHttpServerFactory(object):
-    """
-    Factory for an xray-instrument aiohttp Application's
-    that implements our test endpoints.
-    """
-
-    __test__ = False
-
-    def __init__(self, loop):
-        self._loop = loop
-
-    async def handle_ok(self, request: web.Request) -> web.Response:
-        """
-        Handle / request
-        """
-        if "content_length" in request.query:
-            headers = {"Content-Length": request.query["content_length"]}
-        else:
-            headers = None
-
-        return web.Response(text="ok", headers=headers)
-
-    async def handle_error(self, request: web.Request) -> web.Response:
-        """
-        Handle /error which returns a 404
-        """
-        return web.Response(text="not found", status=404)
-
-    async def handle_unauthorized(self, request: web.Request) -> web.Response:
-        """
-        Handle /unauthorized which returns a 401
-        """
-        raise HTTPUnauthorized()
-
-    async def handle_exception(self, request: web.Request) -> web.Response:
-        """
-        Handle /exception which raises a KeyError
-        """
-        return {}["key"]
-
-    async def handle_delay(self, request: web.Request) -> web.Response:
-        """
-        Handle /delay request
-        """
-        await asyncio.sleep(0.3, loop=self._loop)
-        return web.Response(text="ok")
-
-    def get_app(self) -> web.Application:
-        app = web.Application(middlewares=[middleware])
-        app.router.add_get("/", self.handle_ok)
-        app.router.add_get("/error", self.handle_error)
-        app.router.add_get("/exception", self.handle_exception)
-        app.router.add_get("/unauthorized", self.handle_unauthorized)
-        app.router.add_get("/delay", self.handle_delay)
-
-        return app
-
-    @classmethod
-    def app(cls, loop=None) -> web.Application:
-        return cls(loop=loop).get_app()
 
 
 class CustomStubbedEmitter(UDPEmitter):
