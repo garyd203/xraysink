f2e1b027d9ad11729b316b06e0091bce69bc3a27
Gary Donovan <gary.donovan@oovvuu.com>
2020-07-09 12:23:59 +1000

    Pull out helper functions for recording response

--- src/xraysink/asgi/middleware.py
+++ src/xraysink/asgi/middleware.py
@@ -1,13 +1,26 @@
 """Various X-Ray middleware's for different ASGI-like server frameworks."""
 
-from aiohttp.web_exceptions import HTTPException
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core.models import http
+from aws_xray_sdk.core.models.segment import Segment
+from aws_xray_sdk.core.models.trace_header import TraceHeader
 from aws_xray_sdk.core.utils import stacktrace
 from aws_xray_sdk.ext.util import calculate_sampling_decision
 from aws_xray_sdk.ext.util import calculate_segment_name
 from aws_xray_sdk.ext.util import construct_xray_header
 from aws_xray_sdk.ext.util import prepare_response_header
+
+# See if app-framework-specific exceptions are present, and substitute with a
+# dummy implementation. These are only used for `isinstance` checking within
+# this module, so this is safe.
+try:
+    from aiohttp.web_exceptions import HTTPException as AioHttpWebException
+except ImportError:
+
+    class AioHttpWebException(Exception):
+        def __init__(self, *args, **kwargs):
+            raise TypeError("Fake exception should not be instantiated")
+
 
 # Middleware functions for aiohttp must be decorated with a aiohttp-specific
 # decorator. The decorator itself is fairly innocuous, but we need to work
@@ -73,30 +86,14 @@ async def xray_middleware(request, handler):
     elif hasattr(request, "client") and request.client.host is not None:
         segment.put_http_meta(http.CLIENT_IP, request.client.host)
 
+    # Call next middleware or request handler
     try:
-        # Call next middleware or request handler
         response = await handler(request)
-    except HTTPException as exc:
-        # Non 2XX responses are raised as HTTPExceptions
-        response = exc
-        raise
-    except Exception as err:
-        # Store exception information including the stacktrace to the segment
-        response = None
-        segment.put_http_meta(http.STATUS, 500)
-        stack = stacktrace.get_stacktrace(limit=xray_recorder.max_trace_back)
-        segment.add_exception(err, stack)
+        _record_response(segment, xray_header, response)
+    except Exception as ex:
+        _record_exception(segment, xray_header, ex)
         raise
     finally:
-        if response is not None:
-            segment.put_http_meta(http.STATUS, _get_response_status(response))
-            if "Content-Length" in response.headers:
-                length = int(response.headers["Content-Length"])
-                segment.put_http_meta(http.CONTENT_LENGTH, length)
-
-            header_str = prepare_response_header(xray_header, segment)
-            response.headers[http.XRAY_HEADER] = header_str
-
         xray_recorder.end_segment()
 
     return response
@@ -122,3 +119,40 @@ def _get_response_status(response) -> int:
         # starlette-style
         return response.status_code
     raise TypeError(f"Don't know how to find the path for {type(response)}")
+
+
+def _record_exception(segment: Segment, xray_header: TraceHeader, ex: Exception):
+    """Record an exception from the web app.
+
+    Note that different web frameworks take different approaches to how
+    exceptions are passed back to a user middleware like ours. For example:
+    * aiohttp can use exceptions to signal non-success HTTP responses, which
+      will be visible to middleware.
+    * fastapi explicitly puts it's standard exception handling in front of
+      middleware, so any exceptions are genuine server failures.
+    """
+    # The aiohttp server framework will use exceptions that act as a response.
+    #
+    # Note that the aiohttp client uses a different exception hierarchy, so
+    # these errors will only occur when we are instrumenting an aiohttp
+    # application, regardless of how the application code makes downstream
+    # HTTP requests.
+    if isinstance(ex, AioHttpWebException):
+        _record_response(segment, xray_header, ex)
+        return
+
+    # Default behaviour - assume this is a server error
+    segment.put_http_meta(http.STATUS, 500)
+    stack = stacktrace.get_stacktrace(limit=xray_recorder.max_trace_back)
+    segment.add_exception(ex, stack)
+
+
+def _record_response(segment: Segment, xray_header: TraceHeader, response):
+    """Record a HTTP response from the web app."""
+    segment.put_http_meta(http.STATUS, _get_response_status(response))
+    if "Content-Length" in response.headers:
+        length = int(response.headers["Content-Length"])
+        segment.put_http_meta(http.CONTENT_LENGTH, length)
+
+    header_str = prepare_response_header(xray_header, segment)
+    response.headers[http.XRAY_HEADER] = header_str
