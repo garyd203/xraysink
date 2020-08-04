4e60113fa278a958a3e0abadb58871110bb81c3c
Gary Donovan <gary.donovan@oovvuu.com>
2020-08-04 22:21:10 +1000

    Move recorder fixture into top-level conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import logging
+from unittest.mock import patch
 
 import pytest
+from aws_xray_sdk import global_sdk_config
+from aws_xray_sdk.core.async_context import AsyncContext
+
+from xray_util import get_new_stubbed_recorder
 
 
 @pytest.fixture
@@ -8,3 +13,23 @@ def caplog(caplog):
     """Override the standard caplog fixture to provide a more sensible default level on the root logger."""
     caplog.set_level(logging.DEBUG)
     return caplog
+
+
+@pytest.fixture(scope="function")
+def recorder(event_loop):
+    """
+    Clean up context storage before and after each test run
+    """
+    xray_recorder = get_new_stubbed_recorder()
+    xray_recorder.configure(
+        service="test", sampling=False, context=AsyncContext(loop=event_loop)
+    )
+
+    patcher = patch("xraysink.asgi.middleware.xray_recorder", xray_recorder)
+    patcher.start()
+
+    xray_recorder.clear_trace_entities()
+    yield xray_recorder
+    global_sdk_config.set_sdk_enabled(True)
+    xray_recorder.clear_trace_entities()
+    patcher.stop()
