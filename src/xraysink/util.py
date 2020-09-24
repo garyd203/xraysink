aad057e8779aa41b5e577c50ca9d2e10706ea4f2
Gary Donovan <gary.donovan@oovvuu.com>
2020-09-24 15:13:48 +1000

    #18: has_current_trace() helper function

---
+++ src/xraysink/util.py
@@ -0,0 +1,15 @@
+"""Miscellaneous functions for working with the X-Ray SDK."""
+
+from aws_xray_sdk.core import xray_recorder
+
+
+# noinspection PyProtectedMember
+def has_current_trace() -> bool:
+    """Whether there is currently a trace.
+
+    This is like calling xray_recorder.get_trace_entity(), but without the
+    annoying error handling.
+    """
+    # See authoritative implementation in
+    # aws_xray_sdk.core.context.Context.get_trace_entity()
+    return bool(getattr(xray_recorder.context._local, "entities", None))
