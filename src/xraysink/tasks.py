70b7b0c5bebfab34958265af33088ba7954c0426
Gary Donovan <gary.donovan@oovvuu.com>
2020-09-24 15:16:50 +1000

    #18: Allow background tasks to be started from an existing segment.

--- src/xraysink/tasks.py
+++ src/xraysink/tasks.py
@@ -2,20 +2,33 @@
 
 import inspect
 import re
+from asyncio import ensure_future
 from typing import Optional
 
 import wrapt
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core.models import http
+from aws_xray_sdk.core.models.subsegment import Subsegment
 
 from . import __version__ as xraysink_version
+from .util import has_current_trace
 
 #: URL scheme for the synthetic URL's used by background tasks.
-TASK_SCHEME = "task"
+TASK_SCHEME: str = "task"
+
+#: Format string for the synthetic URL's used by background tasks.
+TASK_URL_FORMAT: str = TASK_SCHEME + "://localhost/{task_path}"
 
 
 def xray_task_async(*, _url_path: Optional[str] = None):
-    """Decorator for a coroutine that starts a new traced background task.
+    """Decorator for a coroutine that executes an independent traced task.
+
+    If there is an existing trace segment, we create a new segment using that
+    segment as the parent (note that we are unable to create a subsegment,
+    because we can't ensure that the subsegment would finish whilst the parent
+    segment is still active; additionally, the purpose of this decorator is
+    to manage *independent* segments), and additionally execute the decorated
+    coroutine in a task. Otherwise, we originate a new trace.
 
     Notes:
         The X-Ray data model doesn't really allow for an internally originated
@@ -35,44 +48,85 @@ def xray_task_async(*, _url_path: Optional[str] = None):
         made-up URL scheme.
 
     Params:
-        annotations: Annotations to attach ot the created segment.
-        metadata: Dictionary of metadata to attach to the created segment.
-        name: Override the name used for the segment (not recommended). By
-            default the X-Ray SDK will use the service name that has been
-            configured for this process.
         _url_path: String to use as the path of the synthetic URL. The default
             value is determined from the name of the decorated function.
     """
+    # Cached task_name to use for every execution of this decorated function
     task_path: Optional[str] = _url_path
     if task_path is not None:
         task_path = task_path.lstrip("/")
 
     @wrapt.decorator
-    async def wrapper(wrapped, instance, args, kwargs):
+    def wrapper(wrapped, instance, args, kwargs):
+        # The wrapper function returned from this decorator is intentionally
+        # non-async. This is because it needs to access the trace context at
+        # the time the function is called, not when the target coroutine is
+        # actually executed.
+
         # Determine task path just once
         nonlocal task_path
         if task_path is None:
-            funcname = wrapped.__name__
-            if funcname.startswith("_") and not funcname.endswith("_"):
-                funcname = re.sub(r"^_+", repl="", string=funcname, count=1)
+            task_path = _get_task_path(wrapped, instance)
 
-            if instance is None:
-                task_path = funcname
-            else:
-                if inspect.isclass(instance):
-                    task_path = "/".join([instance.__name__, funcname])
-                else:
-                    task_path = "/".join([instance.__class__.__name__, funcname])
+        # Execute the target wrapped function
+        if has_current_trace():
+            # Create a minimal subsegment as a parent for executing the task
+            with xray_recorder.in_subsegment(
+                name=f"Create Task: {task_path}", namespace="local"
+            ) as subsegment:
+                subsegment.put_http_meta(
+                    http.URL, TASK_URL_FORMAT.format(task_path=task_path)
+                )
 
-        # Start a segment from scratch (ie. start a new trace)
-        async with xray_recorder.in_segment_async() as segment:
-            # Add background task info to segment as a synthetic HTTP request
-            segment.put_http_meta(http.URL, f"{TASK_SCHEME}://localhost/{task_path}")
-            segment.put_http_meta(http.CLIENT_IP, "127.0.0.1")
-            segment.put_http_meta(
-                http.USER_AGENT, f"BackgroundTask xraysink/{xraysink_version}"
-            )
+                coro = _execute_task_in_segment(
+                    wrapped, args, kwargs, task_path, parent=subsegment
+                )
 
-            return await wrapped(*args, **kwargs)
+                # We always execute the wrapped function in an asyncio task,
+                # because it needs to be in it's own segment and the X-Ray
+                # context is intended to handle only 1 segment at a time.
+                return ensure_future(coro)
+        else:
+            # Start a segment from scratch (ie. start a new trace)
+            return _execute_task_in_segment(wrapped, args, kwargs, task_path)
 
     return wrapper
+
+
+async def _execute_task_in_segment(
+    wrapped, args, kwargs, task_path, parent: Subsegment = None
+):
+    """Execute a wrapped function inside a new X-Ray segment"""
+    # Setup trace context from parent, if necessary
+    segment_params = {}
+    if parent is not None:
+        xray_recorder.clear_trace_entities()
+        segment_params.update(
+            traceid=parent.trace_id, parent_id=parent.id, sampling=parent.sampled
+        )
+
+    # Create a new segment for the task
+    async with xray_recorder.in_segment_async(**segment_params) as segment:
+        # Add background task info to segment as a synthetic HTTP request
+        segment.put_http_meta(http.URL, TASK_URL_FORMAT.format(task_path=task_path))
+        segment.put_http_meta(http.CLIENT_IP, "127.0.0.1")
+        segment.put_http_meta(
+            http.USER_AGENT, f"BackgroundTask xraysink/{xraysink_version}"
+        )
+
+        return await wrapped(*args, **kwargs)
+
+
+def _get_task_path(wrapped, instance) -> str:
+    """Get the synthetic URL path for a task, based on the `wrapt` parameters."""
+    funcname = wrapped.__name__
+    if funcname.startswith("_") and not funcname.endswith("_"):
+        funcname = re.sub(r"^_+", repl="", string=funcname, count=1)
+
+    if instance is None:
+        return funcname
+    else:
+        if inspect.isclass(instance):
+            return "/".join([instance.__name__, funcname])
+        else:
+            return "/".join([instance.__class__.__name__, funcname])
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,18 +1,21 @@
 """Test the background task helpers."""
 
+from asyncio import gather
 from typing import Optional
 from urllib.parse import urlparse
 
 import pytest
+from aws_xray_sdk.core import AsyncAWSXRayRecorder
 from aws_xray_sdk.core.models.segment import Segment
 
+from xraysink.context import AsyncContext
 from xraysink.tasks import xray_task_async
 
 pytestmark = pytest.mark.asyncio
 
 
-class TestXrayTaskAsync:
-    """Test the xray_task_async() decorator."""
+class BaseXrayTaskTests:
+    """Core functionality for testing the xray_task_async() decorator."""
 
     async def _verify_core_segment(
         self, segment: Segment, iserror: bool = False, isfault: bool = False
@@ -53,6 +56,10 @@ class TestXrayTaskAsync:
         assert (
             "response" not in segment.http
         ), "Should not provide any HTTP response data for synthetic request."
+
+
+class TestXrayTaskAsync(BaseXrayTaskTests):
+    """Test the xray_task_async() decorator."""
 
     async def test_should_create_segment_with_synthetic_http_request(self, recorder):
         # Setup SUT function
@@ -156,3 +163,84 @@ class TestXrayTaskAsync:
 
         exception = segment.cause["exceptions"][0]
         assert exception.type == "ValueError"
+
+
+class TestXrayTaskAsyncNested(BaseXrayTaskTests):
+    """Test the xray_task_async() decorator when nested inside an existing segment."""
+
+    @pytest.fixture
+    def recorder(self, recorder) -> AsyncAWSXRayRecorder:
+        # Nested tasks will only work if we are using a well-behaved context
+        recorder.configure(context=AsyncContext())
+        yield recorder
+
+    async def _verify_nested_segment(self, segments, initial_name: str, task_name):
+        """Verify the X-Ray entities created by a nested task."""
+        assert len(segments) == 2, "Should create another segment for the task"
+        initial_segment = [s for s in segments if s.name == initial_name][0]
+        task_segment = [s for s in segments if s is not initial_segment][0]
+
+        # Verify initial segment
+        await self._verify_core_segment(initial_segment)
+        assert (
+            len(initial_segment.subsegments) == 1
+        ), "Should create a subsegment for the inner task."
+
+        # Verify local subsegment
+        subsegment = initial_segment.subsegments[0]
+        assert "Create Task" in subsegment.name
+        assert task_name in subsegment.name
+
+        request_data = subsegment.http["request"]
+        url = urlparse(request_data["url"])
+        assert url.scheme == "task", "Should use synthetic URL scheme"
+        assert (
+            url.netloc == "localhost"
+        ), "Should use localhost for hostname in synthetic URL"
+        assert task_name in url.path, "URL should use task name in synthetic path"
+
+        # Verify task segment
+        await self._verify_core_segment(task_segment)
+        await self._verify_http_segment(task_segment, expected_path="/do_something")
+        assert task_segment.trace_id == initial_segment.trace_id
+        assert task_segment.parent_id == subsegment.id
+
+    async def test_should_set_parent_in_blocking_coroutine(self, recorder):
+        # Setup SUT function
+        @xray_task_async()
+        async def do_something(a, b):
+            return a + b
+
+        # Exercise
+        async with recorder.in_segment_async("initial_segment"):
+            func_result = await do_something(1, 2)
+
+        # Verify
+        assert func_result == 3, "Function result should be returned"
+
+        await self._verify_nested_segment(
+            recorder.emitter.segments,
+            initial_name="initial_segment",
+            task_name="do_something",
+        )
+
+    async def test_should_set_parent_in_asyncio_task(self, recorder):
+        # Setup SUT function
+        @xray_task_async()
+        async def do_something(a, b):
+            return a + b
+
+        # Exercise
+        async with recorder.in_segment_async("initial_segment"):
+            coro = do_something(1, 2)
+
+        func_result, = await gather(coro, return_exceptions=True)
+
+        # Verify
+        assert func_result == 3, "Function result should be returned"
+
+        await self._verify_nested_segment(
+            recorder.emitter.segments,
+            initial_name="initial_segment",
+            task_name="do_something",
+        )
