4c4cb6ab3e265eff8c42d256cfa7455104fe2e4e
Gary Donovan <gdonovan@exoflare.io>
2022-02-23 21:28:42 +1100

    #55: Check comprehensions, commented code, executables

--- src/xraysink/util.py
+++ src/xraysink/util.py
@@ -10,6 +10,5 @@ def has_current_trace() -> bool:
     This is like calling xray_recorder.get_trace_entity(), but without the
     annoying error handling.
     """
-    # See authoritative implementation in
-    # aws_xray_sdk.core.context.Context.get_trace_entity()
+    # See authoritative implementation in aws_xray_sdk.core.context.Context.get_trace_entity()
     return bool(getattr(xray_recorder.context._local, "entities", None))
